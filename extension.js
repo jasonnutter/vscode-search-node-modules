89e079cd2bbb3829dc26a05a2ea0a9f82a9ee5e5
Ori Marron <orim@wix.com>
2019-05-05 17:22:08 +0300

    Add initial support for lerna monorepos

--- extension.js
+++ extension.js
@@ -1,6 +1,7 @@
 const vscode = require('vscode');
 const fs = require('fs');
 const path = require('path');
+const { findLernaPackageDirs } = require('./utils');
 
 var lastFolder = '';
 var lastWorkspaceName = '';
@@ -75,6 +76,56 @@ exports.activate = context => {
             });
         };
 
+        const getProjectFolder = async (workspaceFolder) => {
+            const packages = await findLernaPackageDirs(workspaceFolder.uri.fsPath);
+            // If in a lerna monorepo, prompt user to select which project to traverse
+            if (packages.length > 0) {
+                const selected = await vscode.window.showQuickPick(
+                    [
+                        { label: workspaceFolder.name, packageDir: '' },
+                        ...packages.map(packageDir => ({ label: path.join(workspaceFolder.name, packageDir), packageDir }))
+                    ]
+                    , { placeHolder: 'Select Project' }
+                );
+                if (!selected) {
+                    return;
+                }
+
+                return {
+                    name: selected.label,
+                    path: path.join(workspaceFolder.uri.fsPath, selected.packageDir)
+                };
+            }
+
+            // Otherwise, use the root folder
+            return {
+                name: workspaceFolder.name,
+                path: workspaceFolder.uri.fsPath
+            };
+        };
+
+        const getWorkspaceFolder = async () => {
+            // If in a multifolder workspace, prompt user to select which one to traverse.
+            if (vscode.workspace.workspaceFolders.length > 1) {
+                const selected = await vscode.window.showQuickPick(vscode.workspace.workspaceFolders.map(folder => ({
+                    label: folder.name,
+                    folder
+                })), {
+                    placeHolder: 'Select workspace folder'
+                });
+
+                if (!selected) {
+                    return;
+                }
+
+                return selected.folder;
+            }
+
+            // Otherwise, use the first one
+            const folder = vscode.workspace.workspaceFolders[0];
+            return folder;
+        };
+
         // Open last folder if there is one
         if (useLastFolder && lastFolder) {
             return searchPath(lastWorkspaceName, lastWorkspaceRoot, lastFolder);
@@ -85,24 +136,11 @@ exports.activate = context => {
             return showError('You must have a workspace opened.');
         }
 
-        // If in a multifolder workspace, prompt user to select which one to traverse.
-        if (vscode.workspace.workspaceFolders.length > 1) {
-            vscode.window.showQuickPick(vscode.workspace.workspaceFolders.map(folder => ({
-                label: folder.name,
-                folder
-            })), {
-                placeHolder: 'Select workspace folder'
-            })
-                .then(selected => {
-                    if (selected) {
-                        searchPath(selected.label, selected.folder.uri.fsPath, nodeModulesPath);
-                    }
-                });
-        } else {
-            // Otherwise, use the first one
-            const folder = vscode.workspace.workspaceFolders[0];
-            searchPath(folder.name, folder.uri.fsPath, nodeModulesPath);
-        }
+        getWorkspaceFolder().then(folder => folder && getProjectFolder(folder)).then(folder => {
+            if (folder) {
+                searchPath(folder.name, folder.path, nodeModulesPath);
+            }
+        });
     });
 
     context.subscriptions.push(searchNodeModules);
